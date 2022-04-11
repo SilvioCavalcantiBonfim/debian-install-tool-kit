83d9162c2cfe2580342e3db0f756679997ca5d48
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-04-11 01:40:43 -0300

    rt bluetooth

--- Deb's/rt bluetooth/install.sh
+++ Deb's/rt bluetooth/install.sh
@@ -1,1 +1,10 @@
- 
+#!/bin/bash
+#Criado por: Jonathan Wolff Andrade - 2012
+###Metodo que verifica/instala pacote
+echo "Instalando rtb-dkms.deb."
+sudo dpkg -i rtb-dkms.deb
+echo "."
+sudo sudo modprobe rtbth
+echo -n "Pressione qualquer tecla para sair..."
+read
+exit
